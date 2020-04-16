f967c40b80e19c67e630e183b1058441376a8aa8
Nikhil <nikhilhenry@Nikhils-MBP.Dlink>
2020-04-16 17:36:46 +0530

    get posts

--- pages/index.vue
+++ pages/index.vue
@@ -1,72 +1,27 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        thecovidsurvivalguide
-      </h1>
-      <h2 class="subtitle">
-        My excellent Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div>
+    <main>
+      <h2>LATEST THOUGHTS</h2>
+      <ul>
+        <li v-for="(post,index) in posts" :key = "index">
+          <img :src="post.feature_image">
+          <div class="content">
+            <span>{{ post.authors[0].name}}</span>
+            <nuxt-link :to="{ path: post.slug}">{{post.title}}</nuxt-link>
+            <p>{{post.excerpt}}</p>
+          </div>
+        </li>
+      </ul>
+    </main>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
+import { getPosts } from '~/api/posts';
 export default {
-  components: {
-    Logo
+  async asyncData () {
+    const posts = await getPosts();
+    return { posts: posts }
   }
 }
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
