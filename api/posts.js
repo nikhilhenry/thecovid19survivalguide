a3b5d3b00f286a576f5d4277969b57d52022d4dd
Nikhil <nikhilhenry@Nikhils-MBP.Dlink>
2020-04-16 17:08:44 +0530

    create ghost api function

---
+++ api/posts.js
@@ -0,0 +1,18 @@
+import GhostContentAPI from '@tryghost/content-api';
+
+const api = new GhostContentAPI({
+  url: 'http://ec2-18-130-100-148.eu-west-2.compute.amazonaws.com',
+  key: 'ef89982e83f60ba209f90b97a7',
+  version: "v3"
+});
+
+export async function getPosts(){
+  return await api.posts
+    .browse({
+      limit:"all",
+      include:"tags,authors"
+    })
+    .catch(err=>{
+      console.error(err);
+    });
+}
