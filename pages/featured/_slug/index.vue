2c3d7a8b1be0a425c1345726757b34860cd8571b
Nikhil <nikhilhenry@Nikhils-MBP.Dlink>
2020-05-05 16:33:21 +0530

    style featured

--- pages/featured/_slug/index.vue
+++ pages/featured/_slug/index.vue
@@ -1,14 +1,8 @@
 <template>
 <div>
   <div class="container">
-      <h2 class="date">{{post.date}}</h2>
       <h1 class="title">{{post.title}}</h1>
       <p class="read_time"><i class="fas fa-hourglass-half"></i> {{post.reading_time}} MIN READ</p>
-      <ul class="tags">
-        <li v-for="(tag,index) in post.tags" :key="index">
-          <span>{{tag.name}}</span>
-        </li>
-      </ul>
   </div>
   <figure class="full_image">
   <img :src="post.feature_image" class="cover_image">
@@ -16,7 +10,6 @@
   <div class="container">
     <section class="content">
         <div v-html="post.html" class="text">{{post.html}}</div>
-        <h1 class="author">By {{ post.authors[0].name}}</h1>
     </section>
   </div>
 </div>
@@ -39,7 +32,7 @@ export default {
 
 <style lang="scss" scoped>
 .container{
-  margin-top:1em;
+  padding:1em 0 1em;
 
   .date{
     font-weight: 100;
@@ -52,6 +45,7 @@ export default {
     font-weight: bolder;
     font-size: 3em;
     line-height: 1;
+    margin-top:.5em;
   }
 
   .read_time{
@@ -61,33 +55,11 @@ export default {
     margin: .75em 0 .5em;
   }
 
-  .tags{
-    list-style-type:none;
-    padding-left: 0;
-    display: flex;
-    // justify-content: space-between;
-
-    li{
-      background-color: #eeeeee;
-      display: inline-block;
-      padding: .3em .7em .3em;
-      border-radius: 20px;
-      font-weight: 400;
-      margin:.5em .5em .5em 0;
-    }
-    margin-bottom: 1.5em;
-  }
+}
 
   .content{
-    padding-bottom: 3em;
-    .author{
-    font-weight: bold;
-    color: $primary-color;
-    margin: .75em 0 .5em;
-    }
+    margin-bottom: 4em;
   }
-
-}
 
   .cover_image{
     width:100%;
