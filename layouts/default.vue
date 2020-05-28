3cf0418266d9a788be6074c5145ef00166f5da5d
Nikhil <nikhilhenry@Nikhils-MBP.Dlink>
2020-05-28 22:09:49 +0530

    change up footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,23 +6,22 @@
       <div class="container">
         <div class="columns">
           <div class="column social">
-            <h2>Follow us on social media</h2>
-            <a href="https://www.instagram.com/legacy_chiaroscuro/"><i class="fab fa-instagram"></i></a>
-            <a href="https://www.youtube.com/channel/UCrcwLBYBSELGoHN6OAO7iPw"><i class="fab fa-youtube"></i></a>
-            <a href=""><i class="fab fa-facebook"></i></a>
+            <h2>Connect</h2>
+            <a href="https://www.instagram.com/legacyschoolbangalore/" class="font-awesome"><i class="fab fa-instagram"></i></a>
+            <a href="https://www.youtube.com/channel/UCrcwLBYBSELGoHN6OAO7iPw" class="font-awesome"><i class="fab fa-youtube"></i></a>
+            <a href="https://www.facebook.com/LegacySchoolBangalore/" class="font-awesome"><i class="fab fa-facebook"></i></a>
             <br>
-            <p class="address"><strong>Address: </strong>6/1A, 6/2 Byrathi Village, Bidarahalli Hobli, Off Hennur-Bagalur Main Road (New, International Airport Link Road), Bengaluru, Karnataka 560077</p>
-          </div>
-          <div class="column contact">
-            <h2>Contact Us</h2>
             <ul>
               <h3>TEAM</h3>
               <li><a href="mailto:student.voices@lsb.edu.in"> student.voices@lsb.edu.in</a></li>
               <!-- <h3>UNODC</h3>
               <li><a href="mailto:educationforjustice@unodc.org"> educationforjustice@unodc.org</a></li> -->
             </ul>
+            <p class="address"><strong>Address: </strong>6/1A, 6/2 Byrathi Village, Bidarahalli Hobli, Off Hennur-Bagalur Main Road (New, International Airport Link Road), Bengaluru, Karnataka 560077</p>
+          </div>
+          <div class="column contact">
             <h2>Disclaimer</h2>
-            <p>This website features creative content produced and submitted by students. The views and opinions expressed on this website, and the responsibility of ensuring their originality, are those of the authors. Content on this website does not necessarily reflect the official policy or position of The Legacy School or UNODC. UNODC and The Legacy School do not take any responsibility for any copyright claims or any other disputes that may arise due to any item placed on this website</p>
+            <p>This website features creative content produced and submitted by students. The views and opinions expressed on this website, and the responsibility of ensuring their originality, are those of the authors. Content on this website does not necessarily reflect the official policy or position of Legacy School or UNODC. UNODC and The Legacy School do not take any responsibility for any copyright claims or any other disputes that may arise due to any item placed on this website.</p>
           </div>
         </div>
         <div class="content">
@@ -105,13 +104,23 @@ footer {
     }
 
     .social{
-      a{
+      .font-awesome{
         font-size: 1.9em;
         margin-right: 1em;
       }
 
+      ul{
+        padding-top: .8em;
+        h3{
+          font-size: 1.3em;
+        }
+      }
       p{
        padding-top: .4em; 
+      }
+
+      .address{
+        padding-top:.8em;
       }
     }
 
@@ -130,7 +139,7 @@ footer {
         margin-bottom: 1.5em;
       }
       p{
-        font-size: .8em;
+        font-size: .9em;
       }
     }
 
@@ -160,6 +169,10 @@ footer {
 
     .contact{
       text-align: right;
+      max-width: 30%;
+      p{
+        text-align: justify;
+      }
     }
   }
 
