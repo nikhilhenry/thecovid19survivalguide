e8565a7e7e6e85c3184f8f01da161c9c2afe5c3f
Nikhil <nikhilhenry@Nikhils-MBP.Dlink>
2020-06-03 18:23:06 +0530

    fix issues with youtube embed

--- pages/gallery/_slug/index.vue
+++ pages/gallery/_slug/index.vue
@@ -61,6 +61,19 @@ export default {
     .name{
       padding:0 .5em;
     }
+
+     /deep/.kg-embed-card{
+        position: relative;
+        padding-bottom: 56.25%; /* 16:9 */
+        height: 0;
+     } 
+    /deep/iframe{
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+    }
   }
 
 }
