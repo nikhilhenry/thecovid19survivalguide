7a2cbdfba533518dba114a503bf5f98a015fcbf1
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-06-27 19:05:03 +0530

    setup base content

---
+++ content/posts/big-tech.md
@@ -0,0 +1,27 @@
+---
+title: BIG TECH AND COVID-19
+author: Nikhil Henry
+date: JUN 8 2020
+feature_image: https://web.archive.org/web/20210522201910im_/https://unavocaghost.lsb.edu.in/content/images/2020/06/fabio-oyXis2kALVg-unsplash.jpg
+slug: hello/world
+---
+BIG TECH AND COVID-19	
+
+As the vastly growing global pandemic continues to reshape our societies and lifestyles, the industry powering the 21 century is also undergoing a shift in paradigm. Today, Big Tech faces a startling opportunity to both recorrect its wrongs and a pave new ways for positive collaboration and supplement society.
+
+Medical research papers and countries like South Korea have revealed contact tracing as the secret to combating the virus. Thus, companies such as Apple and Google, fierce rivals in the tech space have collaborated on interconnected solutions leveraging the BlueTooth capabilities of a smartphone. Through this Application Programming Interface they aim to provide governments with a more accessible and low-cost solution to implement contact tracing effectively. Demonstrating that there has never been a more important moment to work together to solve one of the world’s most pressing problems.
+
+The rapid rate of infections continues to increase on a daily basis leading to an evermore demand for medical equipment such as ventilators(essential for providing life support to a high-risk patient). This has lead to the rise in open-source collaboration worldwide amongst engineers, medical professionals, hobbyists and industrial experts. Leveraging the power of 3d printing and cloud-based applications low-cost ventilator designs continue to be published and shared online enabling developing countries to provide health care support to all. These innovations from institutes such as MIT as well the open-source community continue to further showcase the power of open collaboration, especially in a time of crisis. 
+
+The race for solutions towards the virus is on and MNCs are catalysing the process with the help of governments. Companies such as Microsoft, Intel and Amazon Web Services have come forward to lend a helping hand in the Indian Prime minister’s Innovate Challenge on the Coronavirus. The challenge has already received over 7,000 entries consisting of ideas in the field of AI to robotics, all while providing innovators with access to some of the best cloud infrastructure platforms to further fasten the application development process.  “Harnessing innovation for a healthier planet. A lot of people have been sharing technology-driven solutions for Covid-19,” the Indian PM had tweeted, adding, “I would urge them to share them on @mygovindia. These efforts can help many.” Thus, emphasizing the importance of an open and collaborative community.
+
+Crises like COVID-19 further reiterate that humanity’s most powerful tool continues to be collaboration, with the rise of government and corporate collaborations; a ray of hope continues to shine upon mankind.
+
+Sources:
+https://government.economictimes.indiatimes.com/news/technology/tackling-the-virus-big-tech-ecomm-pitch-in-for-pm-innovate-challenge/74807095
+
+https://www.forbesindia.com/article/coronavirus/the-covid19-opportunity-for-big-tech/59105/1
+
+https://www.apple.com/in/newsroom/2020/04/apple-and-google-partner-on-covid-19-contact-tracing-technology/
+
+https://e-vent.mit.edu/
--- pages/index.vue
+++ pages/index.vue
@@ -1,143 +1,141 @@
 <template>
-<div>
-  <Hero v-bind:heroPosts="heroPosts"/>
-  <div class="container">
-    <main>
-      <section class="archive">
-        <article v-for="(post,index) in posts" :key="index" v-on:click="pushToPost(post.slug)">
-          <img :src="post.feature_image">
-          <div class="content">
-            <div class="stuff">
-            <ul class="tags">
-              <li v-for="(tag,index) in post.tags" :key="index">
-                <span class="tag">#{{tag.name}}</span>
-              </li>
-            </ul>
-            <h2 class="title">{{post.title}}</h2>
-            <p>{{post.excerpt}}</p>
+  <div>
+    <Hero v-bind:heroPosts="heroPosts" />
+    <div class="container">
+      <main>
+        <section class="archive">
+          <article
+            v-for="(post, index) in posts"
+            :key="index"
+            v-on:click="pushToPost(post.slug)"
+          >
+            <img :src="post.feature_image" />
+            <div class="content">
+              <div class="stuff">
+                <h2 class="title">{{ post.title }}</h2>
+                <p>{{ post.excerpt }}</p>
+              </div>
+              <span class="author">{{ post.author }}</span>
+              <div class="extra">
+                <span>{{ post.date }} <i class="bullet">&#8226;</i> </span>
+              </div>
             </div>
-            <span class="author">{{post.authors[0].name}}</span>
-            <div class="extra">
-              <span>{{post.date}} <i class="bullet">&#8226;</i> {{post.reading_time}} MIN READ</span>
-            </div>
-          </div>
-        </article>
+          </article>
+        </section>
+      </main>
+      <section class="button">
+        <button class="loadmore" v-on:click="loadMore">load more</button>
       </section>
-    </main>
-    <section class="button">
-      <button class="loadmore" v-on:click="loadMore">load more</button>
-    </section>
+    </div>
   </div>
-</div>
 </template>
 
 <style lang="scss" scoped>
-  main{
-    margin:3em 0 1em;
-  }
-  .archive{
-    display:grid;
-    grid-template-columns: repeat(auto-fit, minmax(340px, 1fr));
-    grid-gap: 2em;
-  }
+main {
+  margin: 3em 0 1em;
+}
+.archive {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(340px, 1fr));
+  grid-gap: 2em;
+}
 
-  main{
-    article{
+main {
+  article {
+    display: flex;
+    flex: 1 1 301px;
+    flex-direction: column;
+    border-bottom: 1px solid #dfe4e7;
+    cursor: pointer;
+    img {
+      height: 200px;
+      width: 100%;
+      border-radius: 0.3em;
+      object-fit: cover;
+    }
+
+    .content {
+      padding: 0.5em 0 0.5em;
+      margin-bottom: 0.5em;
+      flex: 1 1 auto;
       display: flex;
-      flex:1 1 301px;
       flex-direction: column;
-      border-bottom: 1px solid #dfe4e7;
-      cursor: pointer;
-      img{
-        height: 200px;
-        width:100%;
-        border-radius: .3em;
-        object-fit: cover;
-      }
 
-      .content{
-        padding: .5em 0 .5em;
-        margin-bottom: .5em;
+      .stuff {
         flex: 1 1 auto;
-        display: flex;
-        flex-direction: column;
+      }
 
-        .stuff{
-          flex:1 1 auto;
+      ul {
+        list-style-type: none;
+        padding: 0;
+        .tag {
+          color: $tag-color;
+          text-transform: uppercase;
+          margin: 0 0 0.2em;
+          letter-spacing: 0.2px;
+          font-size: 0.8em;
+          font-weight: 400;
         }
-  
-        ul{
-          list-style-type: none;
-          padding: 0;
-          .tag{
-            color: $tag-color;
-            text-transform: uppercase;
-            margin:0 0 .2em;
-            letter-spacing: .2px;
-            font-size: .8em;
-            font-weight: 400;
-          }
-        }
+      }
 
-        .title{
-          margin:.1em 0 .4em;
-          line-height: 1.2em;
-        }
+      .title {
+        margin: 0.1em 0 0.4em;
+        line-height: 1.2em;
+      }
 
-        p{
-          text-align: justify;
-          text-justify: auto;
-          display: block;
-          color:#738a94;
-          margin-bottom:2em;
-          font-size: .9em;
-        }
-        .author{
-          font-weight: 600;
-          color: #424852;
-          font-size: .9em;
-        }
-        
-        .extra{
-          span{
-            color:#92a3ab;
-            font-size: .8em;
-          }
+      p {
+        text-align: justify;
+        text-justify: auto;
+        display: block;
+        color: #738a94;
+        margin-bottom: 2em;
+        font-size: 0.9em;
+      }
+      .author {
+        font-weight: 600;
+        color: #424852;
+        font-size: 0.9em;
+      }
+
+      .extra {
+        span {
+          color: #92a3ab;
+          font-size: 0.8em;
         }
       }
     }
   }
+}
 
-  .button{
-    text-align: center;
-    margin: 0 0 5em;
-    .loadmore{
-      font-size: 1em;
-      text-transform: uppercase;
-      background:none;
-      border: solid 2px rgba($title-color, $alpha: .5);
-      border-radius: 5px;
-      color: rgba($title-color, $alpha: .5);
-      opacity: .8;
-      padding:.2em 1em .2em;
-      transition: background .2s ease-in;
-    }
-    .loadmore:hover{
-      background: $title-color;
-      color:white;
-    }
+.button {
+  text-align: center;
+  margin: 0 0 5em;
+  .loadmore {
+    font-size: 1em;
+    text-transform: uppercase;
+    background: none;
+    border: solid 2px rgba($title-color, $alpha: 0.5);
+    border-radius: 5px;
+    color: rgba($title-color, $alpha: 0.5);
+    opacity: 0.8;
+    padding: 0.2em 1em 0.2em;
+    transition: background 0.2s ease-in;
   }
+  .loadmore:hover {
+    background: $title-color;
+    color: white;
+  }
+}
 
-@media only screen and (min-width: 1000px){
-
-  article:nth-child(7n -6){
+@media only screen and (min-width: 1000px) {
+  article:nth-child(7n -6) {
     grid-column: 1/-1;
-    display:grid;
+    display: grid;
     grid-template-columns: 50% auto;
     grid-column-gap: 1em;
-    img{
-      height:300px;
-      margin:0 0 1em;
+    img {
+      height: 300px;
+      margin: 0 0 1em;
     }
   }
   // article{
@@ -149,47 +147,49 @@
 </style>
 
 <script>
-import {getFeaturedPost} from '~/api/posts';
-import { getPosts } from '~/api/posts';
-import Hero from '~/components/hero';
+import { getFeaturedPost } from "~/api/posts";
+import { getPosts } from "~/api/posts";
+import Hero from "~/components/hero";
 export default {
-  components:{
-    Hero
+  components: {
+    Hero,
   },
-  data(){
-    return{
-      pageNum:1,
-      posts:[]
-    }
+  data() {
+    return {
+      pageNum: 1,
+      posts: [],
+    };
   },
-  async asyncData () {
-    var heroPosts = await getFeaturedPost();
-    var f_posts = await getPosts(1);
-    var posts = [];
-    f_posts.forEach(post => {
-      post.date = new Date(post.published_at).toLocaleString('default', { month: 'short' }).toUpperCase()+' '+new Date(post.published_at).getDate()+' '+new Date(post.published_at).getFullYear();
-      posts.push(post);
-    });
+  async asyncData({ $content }) {
+    const heroPosts = await $content("hero").fetch();
+    const posts = await $content("posts").fetch();
     // console.log(posts)
-    return { posts: posts,heroPosts : heroPosts }
+    return { posts: posts, heroPosts: heroPosts };
   },
-  methods:{
-    pushToPost(slug){
+  methods: {
+    pushToPost(slug) {
       this.$router.push({
-        path:'/blog/'+slug
-      })
+        path: "/blog/" + slug,
+      });
     },
-  async loadMore(){
-    this.pageNum+=1;
-    var f_posts = await getPosts(this.pageNum); 
-    var new_posts = []
-    f_posts.forEach(post => {
-      post.date = new Date(post.published_at).toLocaleString('default', { month: 'short' }).toUpperCase()+' '+new Date(post.published_at).getDate()+' '+new Date(post.published_at).getFullYear();
-      new_posts.push(post);
-    });
-    this.posts = this.posts.concat(new_posts)
-    console.log(this.posts)
+    async loadMore() {
+      this.pageNum += 1;
+      var f_posts = await getPosts(this.pageNum);
+      var new_posts = [];
+      f_posts.forEach((post) => {
+        post.date =
+          new Date(post.published_at)
+            .toLocaleString("default", { month: "short" })
+            .toUpperCase() +
+          " " +
+          new Date(post.published_at).getDate() +
+          " " +
+          new Date(post.published_at).getFullYear();
+        new_posts.push(post);
+      });
+      this.posts = this.posts.concat(new_posts);
+      console.log(this.posts);
+    },
   },
-  }
-}
+};
 </script>
