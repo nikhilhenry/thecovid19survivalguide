90b940b6b5f93a0aa1b1fbce7e58a6fb9a18274e
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-06-27 19:19:15 +0530

    create feature page

--- pages/featured/_slug/index.vue
+++ pages/featured/_slug/index.vue
@@ -1,91 +1,87 @@
 <template>
-<div>
-  <div class="container">
-      <h1 class="title">{{post.title}}</h1>
-      <p class="read_time"><i class="fas fa-hourglass-half"></i> {{post.reading_time}} MIN READ</p>
+  <div>
+    <div class="container">
+      <h1 class="title">{{ post.title }}</h1>
+      <p class="read_time">
+        <i class="fas fa-hourglass-half"></i> {{ post.reading_time }} MIN READ
+      </p>
+    </div>
+    <figure class="full_image">
+      <img :src="post.feature_image" class="cover_image" />
+    </figure>
+    <div class="container">
+      <section class="content">
+        <nuxt-content :document="post" class="text" />
+      </section>
+    </div>
   </div>
-  <figure class="full_image">
-  <img :src="post.feature_image" class="cover_image">
-  </figure>
-  <div class="container">
-    <section class="content">
-        <div v-html="post.html" class="text">{{post.html}}</div>
-    </section>
-  </div>
-</div>
 </template>
 
 <script>
-import { getSinglePost } from '~/api/posts';
+import { getSinglePost } from "~/api/posts";
 export default {
-  async asyncData ({params}) {
-    const post = await getSinglePost(params.slug);
-
-    var d = new Date(post.published_at);
-    var date_string = d.toLocaleString('default', { month: 'long' }).toUpperCase()+' '+d.getDate()+', '+d.getFullYear();
-    post.date = date_string;
-    console.log(post.author)
-    return { post: post }
-  }
-}
+  async asyncData({ $content, params }) {
+    console.log(params);
+    const post = await $content(`/hero/${params.slug}`).fetch();
+    return { post: post };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-.container{
-  padding:1em 0 1em;
+.container {
+  padding: 1em 0 1em;
 
-  .date{
+  .date {
     font-weight: 100;
     color: $tag-color;
-    margin: .75em 0 .5em;
+    margin: 0.75em 0 0.5em;
     font-size: 1.25em;
   }
 
-  .title{
+  .title {
     font-weight: bolder;
     font-size: 3em;
     line-height: 1;
-    margin-top:.5em;
+    margin-top: 0.5em;
   }
 
-  .read_time{
+  .read_time {
     font-size: 1.25em;
     color: #333333;
-    opacity: .5;
-    margin: .75em 0 .5em;
-  }
-
-}
-
-  .content{
-    margin-bottom: 4em;
-  }
-
-  .cover_image{
-    width:100%;
-    height: auto;
-  }
-
-@media only screen and(min-width:900px){
-  .container{
-    width:50%;
-  }
-
-  .cover_image{
-    width:60%!important;
+    opacity: 0.5;
+    margin: 0.75em 0 0.5em;
   }
 }
 
-@media only screen and(min-width: 768px){
+.content {
+  margin-bottom: 4em;
+}
 
-  .full_image{
+.cover_image {
+  width: 100%;
+  height: auto;
+}
+
+@media only screen and(min-width:900px) {
+  .container {
+    width: 50%;
+  }
+
+  .cover_image {
+    width: 60% !important;
+  }
+}
+
+@media only screen and(min-width: 768px) {
+  .full_image {
     display: flex;
     flex-direction: column;
     align-items: center;
     overflow: hidden;
   }
-  .cover_image{
-    width:90%;
+  .cover_image {
+    width: 90%;
   }
 }
 </style>
