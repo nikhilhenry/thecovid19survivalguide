7d9335298cb9560cba927a66328f068865e23bda
Nikhil <25770025+nikhilhenry@users.noreply.github.com>
2022-06-27 18:46:10 +0530

    install nuxt content

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,62 +1,52 @@
-
 export default {
-  mode: 'universal',
+  mode: "universal",
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: "Una Voca" || '',
+    title: "Una Voca" || "",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || "",
+      },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ],
-    script: [
-      {src:"https://kit.fontawesome.com/a40ef71e31.js"}
-    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    script: [{ src: "https://kit.fontawesome.com/a40ef71e31.js" }],
   },
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
   /*
-  ** Global CSS
-  */
-  css: [
-  ],
+   ** Global CSS
+   */
+  css: [],
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
   /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-  ],
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [],
   /*
-  ** Nuxt.js modules
-  */
-  modules: [
-    '@nuxtjs/style-resources'
-  ],
+   ** Nuxt.js modules
+   */
+  modules: ["@nuxtjs/style-resources", "@nuxt/content"],
   styleResources: {
-    scss: [
-      '~assets/scss/_colors.scss',
-      '~hooper/dist/hooper.css'
-    ]
+    scss: ["~assets/scss/_colors.scss", "~hooper/dist/hooper.css"],
   },
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-    ** You can extend webpack config here
-    */
-    extend (config, ctx) {
-    }
-  }
-}
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {},
+  },
+};
